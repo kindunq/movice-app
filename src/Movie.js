0bc29f1dc7b31121a545c8e2300b7eedbbf61861
kindunq <humanb2box@gmail.com>
2018-12-17 18:07:52 +0900

    feat: Add prop-types

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
+import propTypes from 'prop-types';
 import './Movie.css';
 
 class Movie extends Component{
+    static propTypes = {
+        title: propTypes.string.isRequired,
+        poster: propTypes.string.isRequired
+    }
     render(){
        return(
            <div>
@@ -14,6 +19,10 @@ class Movie extends Component{
 
 
 class MoviePoster extends Component {
+    static propTypes = {
+        title: propTypes.string.isRequired,
+        poster: propTypes.string.isRequired
+    }
     render(){
         return(
             <img src={this.props.poster} alt={this.props.title} />
