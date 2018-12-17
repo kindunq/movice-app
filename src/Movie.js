955577a3c63470193bf32afc26593ad0cd6124a9
kindunq <humanb2box@gmail.com>
2018-12-17 16:30:01 +0900

    docs: create movieposter

--- src/Movie.js
+++ src/Movie.js
@@ -4,9 +4,22 @@ import './Movie.css';
 class Movie extends Component{
     render(){
        return(
-        <h1>hello this is a movie</h1>
+           <div>
+                <MoviePoster/>
+                <h1>hello this is a movie</h1>
+            </div>
         )
     }
 }
+
+
+class MoviePoster extends Component {
+    render(){
+        return(
+            <img src="http://pbs.twimg.com/media/CQrmQp5WgAAWtQC.jpg" alt="헝거게임"/>
+        )
+    }    
+}
+
 
 export default Movie;
