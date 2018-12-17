e14bd541f2d4524bd280fac0cd2f782b2ac6fd01
kindunq <humanb2box@gmail.com>
2018-12-17 16:17:05 +0900

    docs: Create movie.js and css

---
+++ src/Movie.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Movie.css';
+
+class Movie extends Component{
+    render(){
+       return(
+        <h1>hello this is a movie</h1>
+        )
+    }
+}
+
+export default Movie;
