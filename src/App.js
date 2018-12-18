292649403804d7fbe779d78aaf291c5803a9f673
kindunq <humanb2box@gmail.com>
2018-12-18 17:01:05 +0900

    docs: Add loading state to _renderMovies

--- src/App.js
+++ src/App.js
@@ -8,48 +8,45 @@ class App extends Component {
   // Render : componentWillMount() -> render() -> componentDidMount()
   // Update : componentWillReceiveProps() -> shouldComponentUpdate()-> componentWillUpdate() -> render() -> componentDidUpdate()
   state = {
-    greeting: 'hello!',
-    movies: [
-      {
-        title:'Matrix',
-        poster:'https://cdn3-www.comingsoon.net/assets/uploads/2018/03/dee4528e57d45839b54d53d0ea27c010.jpg',
-      },
-      {
-        title:'Full Metal Jaket',
-        poster:'https://cinema.cornell.edu/Fall2018/images/full-metal-jacket.jpg',
-      },
-      {
-        title:'Oldboy',
-        poster:'https://i.kinja-img.com/gawker-media/image/upload/s--lGW5m0gy--/c_scale,f_auto,fl_progressive,q_80,w_800/i2eoqko3y4azntzc6fon.jpg',
-      },
-      {
-        title:'Star Wars',
-        poster:'https://cdn3.movieweb.com/i/article/IwOYVaoJz2xThk854h3ybec9UYm9xt/798:50/Star-Wars-9-Rumor-Darth-Vader-Return.jpg',
-      },
-    ]
+    
   }
 
   componentDidMount(){
     setTimeout(() => {
       this.setState({
-        movies: [
-          ...this.state.movies,
+        movies:[
           {
-            title:'Transpotting',
-            poster:'https://homemcr.org/app/uploads/2011/04/Trainspotting-781x460.jpg',
-          }
+            title:'Matrix',
+            poster:'https://cdn3-www.comingsoon.net/assets/uploads/2018/03/dee4528e57d45839b54d53d0ea27c010.jpg',
+          },
+          {
+            title:'Full Metal Jaket',
+            poster:'https://cinema.cornell.edu/Fall2018/images/full-metal-jacket.jpg',
+          },
+          {
+            title:'Oldboy',
+            poster:'https://i.kinja-img.com/gawker-media/image/upload/s--lGW5m0gy--/c_scale,f_auto,fl_progressive,q_80,w_800/i2eoqko3y4azntzc6fon.jpg',
+          },
+          {
+            title:'Star Wars',
+            poster:'https://cdn3.movieweb.com/i/article/IwOYVaoJz2xThk854h3ybec9UYm9xt/798:50/Star-Wars-9-Rumor-Darth-Vader-Return.jpg',
+          },
         ]
       })
-    }, 5000)
+    }, 2000)
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map( (movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
   }
 
   render() {
     return (
       <div className="App">
-      {this.state.greeting}
-        {this.state.movies.map( (movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     )
   }
