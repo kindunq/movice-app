037a37acf503feea4e792c5eb8af9aa86c45f54c
kindunq <humanb2box@gmail.com>
2018-12-18 16:18:27 +0900

    docs: Add ...this.state (전개연산자)

--- src/App.js
+++ src/App.js
@@ -2,37 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title:'Matrix',
-    poster:'https://cdn3-www.comingsoon.net/assets/uploads/2018/03/dee4528e57d45839b54d53d0ea27c010.jpg',
-  },
-  {
-    title:'Full Metal Jaket',
-    poster:'https://cinema.cornell.edu/Fall2018/images/full-metal-jacket.jpg',
-  },
-  {
-    title:'Oldboy',
-    poster:'https://i.kinja-img.com/gawker-media/image/upload/s--lGW5m0gy--/c_scale,f_auto,fl_progressive,q_80,w_800/i2eoqko3y4azntzc6fon.jpg',
-  },
-  {
-    title:'Star Wars',
-    poster:'https://cdn3.movieweb.com/i/article/IwOYVaoJz2xThk854h3ybec9UYm9xt/798:50/Star-Wars-9-Rumor-Darth-Vader-Return.jpg',
-  },
-]
 
 class App extends Component {
 
   // Render : componentWillMount() -> render() -> componentDidMount()
   // Update : componentWillReceiveProps() -> shouldComponentUpdate()-> componentWillUpdate() -> render() -> componentDidUpdate()
   state = {
-    greeting: 'hello!'
+    greeting: 'hello!',
+    movies: [
+      {
+        title:'Matrix',
+        poster:'https://cdn3-www.comingsoon.net/assets/uploads/2018/03/dee4528e57d45839b54d53d0ea27c010.jpg',
+      },
+      {
+        title:'Full Metal Jaket',
+        poster:'https://cinema.cornell.edu/Fall2018/images/full-metal-jacket.jpg',
+      },
+      {
+        title:'Oldboy',
+        poster:'https://i.kinja-img.com/gawker-media/image/upload/s--lGW5m0gy--/c_scale,f_auto,fl_progressive,q_80,w_800/i2eoqko3y4azntzc6fon.jpg',
+      },
+      {
+        title:'Star Wars',
+        poster:'https://cdn3.movieweb.com/i/article/IwOYVaoJz2xThk854h3ybec9UYm9xt/798:50/Star-Wars-9-Rumor-Darth-Vader-Return.jpg',
+      },
+    ]
   }
 
   componentDidMount(){
     setTimeout(() => {
       this.setState({
-        greeting:'hello dongu'
+        movies: [
+          ...this.state.movies,
+          {
+            title:'Transpotting',
+            poster:'https://homemcr.org/app/uploads/2011/04/Trainspotting-781x460.jpg',
+          }
+        ]
       })
     }, 5000)
   }
@@ -41,7 +47,7 @@ class App extends Component {
     return (
       <div className="App">
       {this.state.greeting}
-        {movies.map( (movie, index) => {
+        {this.state.movies.map( (movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
