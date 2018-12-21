591fa6234cb4981574aa3be658ce83ebf3776112
kindunq <humanb2box@gmail.com>
2018-12-21 18:34:23 +0900

    docs: modify movie prop

--- src/App.js
+++ src/App.js
@@ -16,7 +16,13 @@ class App extends Component {
 
   _renderMovies = () => {
     const movies = this.state.movies.map( movie => {
-      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id} />
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres={movie.genres} 
+        synopsis={movie.synopsis}
+      />
     })
     return movies
   }
