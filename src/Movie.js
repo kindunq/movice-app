93326fb8e6dc1a77b039e8cf3a8ba4a5af2676dc
kindunq <humanb2box@gmail.com>
2018-12-21 18:35:18 +0900

    docs: Add props genres, synopsis

--- src/Movie.js
+++ src/Movie.js
@@ -5,23 +5,44 @@ import './Movie.css';
 class Movie extends Component{
     static propTypes = {
         title: propTypes.string.isRequired,
-        poster: propTypes.string.isRequired
+        poster: propTypes.string.isRequired,
+        genres: propTypes.string.isRequired,
+        synopsis: propTypes.string.isRequired
     }
     render(){
        return(
-           <div>
-                <MoviePoster poster={this.props.poster} title={this.props.title}/>
-                <h1>{this.props.title}</h1>
+           <div className="Movie">
+                <div className="Movie__Columns">
+                    <MoviePoster poster={this.props.poster} title={this.props.title}/>
+                </div>
+                <div className="Movie__Columns">
+                    <h1>{this.props.title}</h1>
+                    <div className="Movie__Genres">
+                         {this.props.genres.map ((genre, index) => <MovieGenres genres={genre} key={index} /> )}
+                    </div>
+                </div>
+                
             </div>
         )
     }
 }
 
 
+class MovieGenres extends Component{
+    render(){
+        return(
+            <span className="Movie__Genres">{this.props.genres}</span>
+        )
+    }    
+}
+
+
 class MoviePoster extends Component {
     static propTypes = {
         title: propTypes.string.isRequired,
-        poster: propTypes.string.isRequired
+        poster: propTypes.string.isRequired,
+        genres: propTypes.string.isRequired,
+        synnopsis: propTypes.string.isRequired
     }
     render(){
         return(
