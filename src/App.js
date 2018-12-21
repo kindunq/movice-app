aef2a59233b44299886cb3e27a0d76a24dfcb25a
kindunq <humanb2box@gmail.com>
2018-12-21 16:40:58 +0900

    docs: Add Async Await for _getMovies

--- src/App.js
+++ src/App.js
@@ -11,17 +11,28 @@ class App extends Component {
   state = {}
 
   componentDidMount(){
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+    this._getMovies();
   }
 
   _renderMovies = () => {
     const movies = this.state.movies.map( (movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+      return <Movie title={movie.title} poster={movie.medium_cover_image} key={index} />
     })
     return movies
+  }
+
+   _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render() {
