774f19d01ab7d90b72e597b19ae88020c4b335fb
kindunq <humanb2box@gmail.com>
2018-12-21 16:00:26 +0900

    feat: Add func fetch, then, catch for use 'yts api'

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ class App extends Component {
   state = {}
 
   componentDidMount(){
-    console.log(fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating'))
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
