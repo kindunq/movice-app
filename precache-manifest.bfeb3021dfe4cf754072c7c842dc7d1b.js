805d4e5189392879b9c88040e00fb8cc57294be0
kindunq <humanb2box@gmail.com>
2018-12-27 13:54:33 +0900

    Updates

---
+++ precache-manifest.bfeb3021dfe4cf754072c7c842dc7d1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0a92258fff7ea8b638f",
+    "url": "/movice-app/static/js/runtime~main.d0a92258.js"
+  },
+  {
+    "revision": "cf423ed6f9a6526adaca",
+    "url": "/movice-app/static/js/main.cf423ed6.chunk.js"
+  },
+  {
+    "revision": "bdbcfc72cb0207b3867a",
+    "url": "/movice-app/static/js/1.bdbcfc72.chunk.js"
+  },
+  {
+    "revision": "cf423ed6f9a6526adaca",
+    "url": "/movice-app/static/css/main.822d29fc.chunk.css"
+  },
+  {
+    "revision": "f57e10cd3d646ca65a8afffd95e7d8d9",
+    "url": "/movice-app/index.html"
+  }
+];
